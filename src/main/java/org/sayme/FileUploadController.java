9971a75a4e2f9f78c734f98972214d9a97e2c5ae
zhaomo0823@gmail.com <zhaomo0823@gmail.com>
2021-04-27 23:13:07 +0800

    Initial commit

---
+++ src/main/java/org/sayme/FileUploadController.java
@@ -0,0 +1,49 @@
+package org.sayme;
+/*
+ @user  zhaomo
+ @create 2021-04-27-22:24
+ @Name   file_upload  
+*/
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("/yyyy/MM/dd/");
+    @PostMapping("/upload")
+    public Map<String,Object> fileupload(MultipartFile file, HttpServletRequest req) {
+        Map<String, Object> result = new HashMap<>();
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/") + format;
+        File folder = new File(realPath);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        String oldName = file.getOriginalFilename();
+        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."));
+        try {
+            file.transferTo(new File(folder, newName));
+            String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + format + newName;
+            result.put("status", "OK");
+            result.put("name", oldName);
+            result.put("url", url);
+        } catch (IOException e) {
+            result.put("status", "ERROR");
+            result.put("msg", e.getMessage());
+        }
+        return result;
+    }
+
+}
